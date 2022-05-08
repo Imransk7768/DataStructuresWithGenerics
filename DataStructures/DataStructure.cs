57a132c6aab66d6e89c4731a600bc1168be7afa3
Imransk7768 <imransk7768@gmail.com>
2022-05-09 02:36:25 +0530

    [Imran] Added: Multiple operation of linked list data1

---
+++ DataStructures/DataStructure.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class DataStructure<T>
+    {
+        public Node<T> head;
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine( node.data);
+        }
+        public void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("No Data In LinkedList");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public void AddInReverseOrder(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (head == null)
+            {
+                head = newNode; 
+            }
+            else
+            {
+                Node<T> temp = head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+        public int Search(T value)
+        {
+            Node<T> node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+
+                if (node.data.Equals(value))
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
+
+        public Node<T> InsertAtParticularPosition(int position, T data)
+        {
+            Node<T> newestNode = new Node<T>(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node<T> prev = null;
+            Node<T> current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            Console.WriteLine("Data Inserted in Node");
+            return this.head;
+        }
+        public Node<T> RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            Console.WriteLine("1st Node is Deleted ");
+            return this.head;
+        }
+        public Node<T> RemoveLastNode()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node<T> NewNode = head;
+            while (NewNode.next.next != null)
+            {
+                NewNode = NewNode.next;
+            }
+            NewNode.next = null;
+            Console.WriteLine("Last Node Is Deleted.");
+            return head;
+        }
+        public void DeleteNodeAtParticularPosition(int position)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("No Data Present In LinkedList.");
+                return;
+            }
+            Node<T> temp = this.head;
+            if (position == 0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for (int i = 0; temp != null && i < position - 1; i++)
+            {
+                temp = temp.next;
+            }
+            if (temp == null || temp.next == null)
+            {
+                return;
+            }
+            Node<T> next = temp.next.next;
+            temp.next = next;
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,6 +6,85 @@ namespace DataStructures
         public static void Main(string[] args)
         {
             Console.WriteLine(">>>>  Welcome to DataStructures Using Generics  <<<<");
+            bool end = true;
+            Console.WriteLine("1. Add Data\n2. In Reverse Data\n3. Insert Data\n4. Remove First Node Element\n5. Remove Last Node Element\n6. Search Node\n" +
+                "7. Insert New Data\n8. Delete Node At Particular Position\nEnd Of Program ");
+            while (end)
+            {
+                DataStructure<string> structure = new DataStructure<string>();
+                Console.WriteLine("Enter Option For Exicute The Program");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Add into Linked List");
+                        structure.Add("Rehan");
+                        structure.Add("Ashok");
+                        structure.Add("Sirish");
+                        structure.Display();
+                        break;
+                    case 2:
+                        structure.AddInReverseOrder("Rehan");
+                        structure.AddInReverseOrder("Ashok");
+                        structure.Add("Rehan");
+                        structure.Add("Sirish");
+                        structure.Display();
+                        break;
+                    case 3:
+                        structure.Add("Sirish");
+                        structure.Add("Rehan");
+                        int num = structure.Search("Sirish");
+                        structure.InsertAtParticularPosition(num + 1, "Ashok");
+                        structure.Display();
+                        break;
+                    case 4:
+                        structure.Add("Sirish");
+                        structure.Add("Ashok");
+                        structure.Add("Rehan");
+                        structure.RemoveFirstNode();
+                        structure.Display();
+                        break;
+                    case 5:
+                        structure.Add("Sirish");
+                        structure.Add("Ashok");
+                        structure.Add("Rehan");
+                        structure.RemoveLastNode();
+                        structure.Display();
+                        break;
+                    case 6:
+                        structure.Add("Sirish");
+                        structure.Add("Ashok");
+                        structure.Add("Rehan");
+                        structure.Search("Ashok");
+                        break;
+                    case 7:
+                        structure.Add("Sirish");
+                        structure.Add("Ashok");
+                        structure.Add("Rehan");
+                        int newNode = structure.Search("Ashok");
+                        structure.InsertAtParticularPosition(newNode + 1, "Anil");
+                        structure.Display();
+                        break;
+                    case 8:
+                        structure.Add("Sirish");
+                        structure.Add("Ashok");
+                        structure.Add("Rehan");
+                        int node = structure.Search("Ashok");
+                        structure.InsertAtParticularPosition(node + 1, "Anil");
+                        structure.Display();
+                        Console.WriteLine("\nElement Delete SuccessFully");
+                        structure.DeleteNodeAtParticularPosition(node + 1);
+                        structure.Display();
+                        break;
+                    case 9:
+                        end = false;
+                        Console.WriteLine("Program Is Ended.");
+                        break;
+                    default:
+                        Console.WriteLine("Enter The Correct Option");
+                        break;
+                }
+            }
         }
     }
 }
