1fa5c55fc26c9db869711c50edc4751c063242d1
Imransk7768 <imransk7768@gmail.com>
2022-05-08 23:42:09 +0530

    [Imran] Added: Welcome Message

---
+++ DataStructures/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DataStructures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>  Welcome to DataStructures Using Generics  <<<<");
+        }
+    }
+}
